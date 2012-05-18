fdf19791c56684d040562eb2ce94709ffac92bae
JR Boyens <jboyens@fooninja.org>
2012-05-18 00:19:45 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comics::Application.config.secret_token = 'a7d661f8a70650b1dcdf4d31380b618bb95d6978e35690ac722aa6ee3d7b085b2d542e3b27baffe60339d331d95f64c77543367f040cafe63a450c62707e726a'
