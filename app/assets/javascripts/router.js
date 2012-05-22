b435f269d669221b28a989d7b0e99df9f74e34d7
JR Boyens <jboyens@fooninja.org>
2012-05-22 13:10:33 -0700

    Route links to show a single comic

--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,3 +1,13 @@
 (function() {
-  window.router = Backbone.Router.extend({});
+  window.router = Backbone.Router.extend({
+    routes: {
+      "comic/:path": "show"
+    },
+
+    show: function(path) {
+      console.log("SHOW "+path);
+    }
+  });
+
+  window.app = new window.router;
 })();
