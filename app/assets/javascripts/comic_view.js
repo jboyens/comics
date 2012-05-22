0d7191eb9ac9236b1b68ff00c18e8f4a269bcf2e
JR Boyens <jboyens@fooninja.org>
2012-05-22 13:07:50 -0700

    Implement rendering a single comic

--- app/assets/javascripts/comic_view.js
+++ app/assets/javascripts/comic_view.js
@@ -1,3 +1,19 @@
 (function() {
-  window.views.comic = Backbone.View.extend({});
+  window.views.comic = Backbone.View.extend({
+    tagName: 'li',
+
+    events: {
+      "click .comic_link": "open"
+    },
+
+    open: function() {
+      window.app.navigate("comic/"+this.model.get('path'));
+    },
+
+    render: function() {
+      this.$el.append("<a href=\"#\" class=\"comic_link\">"+this.model.get('path')+"</a>");
+
+      return this;
+    }
+  });
 })();
--- spec/javascripts/comic_view_spec.js
+++ spec/javascripts/comic_view_spec.js
@@ -2,4 +2,19 @@ describe("Comic View", function() {
   it("should be defined", function() {
     expect(window.views.comic).toBeDefined();
   });
+
+  describe("when initialized", function() {
+    var view;
+
+    beforeEach(function() {
+      comic = new window.models.comic({path: '/test.cbr'});
+      view = new window.views.comic({model: comic});
+    });
+
+    it("should show the path in the rendered output", function() {
+      var rendered = view.render().$el;
+
+      expect(rendered.html()).toMatch("test.cbr");
+    });
+  });
 });
